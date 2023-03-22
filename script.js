f1285d015754519e676a6e9414abc599e30804b0
LeganV9 <yanisrenaud77@gmail.com>
2023-03-22 12:52:17 +0100

    upd

---
+++ script.js
@@ -0,0 +1,61 @@
+// ligne 2 declare constante en selectionnant la checkbox (queryselect au lieu de getelementbyid car pas d'id) (etape 1)
+const toggleSwitch = document.querySelector('input[type="checkbox"]')
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+
+// dark or light images using ``so that it is a variable and using ${color} so that it can changes
+function imageMode(color) {
+    image1.src = `img/undraw_proud_coder_${color}.svg`;
+    image2.src = `img/undraw_feeling_proud_${color}.svg`;
+    image3.src = `img/undraw_conceptual_idea_${color}.svg`;
+}
+
+// dark Mode styles (3)
+function darkMode() {
+    nav.style.backgroundColor = 'rgb(0 0 0 / 50%';
+    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%';
+    toggleIcon.children[0].textContent = 'Dark Mode';
+    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+    imageMode('dark');
+}
+
+// light Mode styles (3)
+function lightMode() {
+    nav.style.backgroundColor = 'rgb(255 255 255 / 50%';
+    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%';
+    toggleIcon.children[0].textContent = 'Light Mode';
+    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    imageMode('light');
+}
+
+
+// switch theme dynamically (3)
+function switchTheme(event) {
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark')
+        darkMode();
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light')
+        lightMode();
+    }
+}
+
+// event listener (2)
+toggleSwitch.addEventListener('change', switchTheme);
+
+// check local storage for theme
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) { // here you want to see if the value exists before tying to retrieve it
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        darkMode();
+    }
+} 
